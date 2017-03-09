a9d8de9c6047549a8cb624a167b5443dcb6e24c0
aok7 <aok7@hi.is>
2017-03-09 21:52:10 +0000

    Nýr background og Log out

--- app/src/main/java/is/hopur8/braskarinn/FrontActivity.java
+++ app/src/main/java/is/hopur8/braskarinn/FrontActivity.java
@@ -61,7 +61,7 @@ public class FrontActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
             return true;
         }
 
