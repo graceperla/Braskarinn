e23936c809b6728f1e27a2623e04e23f77817d07
Anton <toni0191@hotmail.com>
2017-03-13 14:33:25 +0000

    XML form á Register

--- app/src/main/java/is/hopur8/braskarinn/RegisterActivity.java
+++ app/src/main/java/is/hopur8/braskarinn/RegisterActivity.java
@@ -2,6 +2,7 @@ package is.hopur8.braskarinn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void register_register(View view) {
+        //RegisterTakki
     }
 }
