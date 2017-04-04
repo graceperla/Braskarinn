748814cb8c1f0b8b031a236919a7edcba75c784c
Sindri Fannar <sindrifv@gmail.com>
2017-04-04 00:42:58 +0000

    Útlits pælingar bara! innihalda einhverjar Warnings, en ekki Errors.! Laga seinna

--- app/src/main/java/is/hopur8/braskarinn/ProfileActivity.java
+++ app/src/main/java/is/hopur8/braskarinn/ProfileActivity.java
@@ -52,10 +52,18 @@ public class ProfileActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
 
         if (id == R.id.action_aboutus) {
+
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+
             return true;
         }
 
         if (id == R.id.action_faq) {
+
+            Intent intent = new Intent(this, FaqActivity.class);
+            startActivity(intent);
+
             return true;
         }
 
